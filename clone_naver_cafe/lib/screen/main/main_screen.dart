f1cdf3ff21a4b8bb7d2a08ffbfc0b38f4fa7b02d
hyun0914 <gus443@naver.com>
2024-05-24 23:14:35 +0900

    [Style] 메인화면에서 주제별 인기 카페, 하단버튼 5개 부분 UI코드 추가

--- clone_naver_cafe/lib/screen/main/main_screen.dart
+++ clone_naver_cafe/lib/screen/main/main_screen.dart
@@ -119,58 +119,7 @@ class MainScreen extends StatelessWidget {
               const SizedBox(height: 10,),
               Container(
                 width: double.infinity,
-                height: 20,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade400,
-                ),
-              ),
-              const SizedBox(height: 10,),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Column(
-                  children: [
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Text('달서구 이웃 소식', style: TextStyle(fontSize: 18),),
-                            SizedBox(width: 8,),
-                            Icon(Icons.my_location),
-                          ],
-                        ),
-                        Text('더보기', style: TextStyle(fontSize: 15),),
-                      ],
-                    ),
-                    const SizedBox(height: 8,),
-                    ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: 4,
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text('이웃', style: TextStyle(fontSize: 16, color: Colors.green),),
-                                SizedBox(width: 4,),
-                                Text('시를 좋아하시거나 사람들과 함께 시에 관해 이야...', style: TextStyle(fontSize: 16),),
-                              ],
-                            ),
-                            Text('서소문동', style: TextStyle(fontSize: 14),),
-                            const SizedBox(height: 8,),
-                          ],
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 2,),
-              Container(
-                width: double.infinity,
-                height: 20,
+                height: 14,
                 decoration: BoxDecoration(
                   color: Colors.grey.shade400,
                 ),
@@ -223,14 +172,239 @@ class MainScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 2,),
+              Container(
+                width: double.infinity,
+                height: 14,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                ),
+              ),
               Container(
                 width: double.infinity,
                 height: 60,
                 color: Colors.greenAccent,
                 child: const Center(child: Text('광고'),),
               ),
+              Container(
+                width: double.infinity,
+                height: 14,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                ),
+              ),
               const SizedBox(height: 10,),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text('달서구 이웃 소식', style: TextStyle(fontSize: 18),),
+                            SizedBox(width: 8,),
+                            Icon(Icons.my_location),
+                          ],
+                        ),
+                        Text('더보기', style: TextStyle(fontSize: 15),),
+                      ],
+                    ),
+                    const SizedBox(height: 8,),
+                    ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 4,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text('이웃', style: TextStyle(fontSize: 16, color: Colors.green),),
+                                SizedBox(width: 4,),
+                                Text('시를 좋아하시거나 사람들과 함께 시에 관해 이야...', style: TextStyle(fontSize: 16),),
+                              ],
+                            ),
+                            Text('서소문동', style: TextStyle(fontSize: 14),),
+                            const SizedBox(height: 8,),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: double.infinity,
+                height: 14,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('주제별 인기 카페', style: TextStyle(fontSize: 18),),
+                    const SizedBox(height: 8,),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                      child: ListView.builder(
+                        itemCount: 25,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+                                decoration: const BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                                ),
+                                child: const Text('가족/육아', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),),
+                              ),
+                              const SizedBox(width: 6,),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 4,),
+                    ListView.separated(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 20,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 8),
+                          child: Container(
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                        );
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text('${index+1}'),
+                                const SizedBox(width: 8,),
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.amber,
+                                    borderRadius: BorderRadius.all(Radius.circular(24))
+                                  ),
+                                ),
+                                const SizedBox(width: 8,),
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('카폐명', style: TextStyle(fontSize: 16),),
+                                    SizedBox(width: 300, child: Text('영화/연극/공연등 티켓 교환 더 즐겁게 디즈니 플러스와도 함께 해요', style: TextStyle(fontSize: 12),)),
+                                    SizedBox(height: 4,),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.person, size: 14,),
+                                        Text('32,000', style: TextStyle(fontSize: 12),),
+                                        SizedBox(width: 4,),
+                                        Text('가지 4단계', style: TextStyle(fontSize: 12),),
+                                        Icon(Icons.arrow_circle_up, size: 14,),
+                                        SizedBox(width: 4,),
+                                        Text('2,700', style: TextStyle(fontSize: 12),),
+                                      ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 30),
+                      child: Container(
+                        width: double.infinity,
+                        height: 40,
+                        decoration: const BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                        ),
+                        child: const Center(child: Text('더보기', style: TextStyle(color: Colors.white),)),
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: 3,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 30,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        color: Colors.blueGrey,
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: const Icon(Icons.back_hand, size: 18,),
+                                    ),
+                                    SizedBox(width: 4,),
+                                    const Text('네이버 대표 카페', style: TextStyle(fontSize: 18),),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: 2,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 30,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        color: Colors.blueGrey,
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: const Icon(Icons.back_hand, size: 18,),
+                                    ),
+                                    SizedBox(width: 4,),
+                                    const Text('네이버 대표 카페', style: TextStyle(fontSize: 18),),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40,),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
